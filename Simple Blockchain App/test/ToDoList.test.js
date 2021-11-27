faa5e89b62872d2b37d8d9062da2c124fdbd9f57
phucding <83810975+phucding@users.noreply.github.com>
2021-11-27 22:14:28 +0700

    Update ToDoList.test.js

--- Simple Blockchain App/test/ToDoList.test.js
+++ Simple Blockchain App/test/ToDoList.test.js
@@ -1,8 +1,13 @@
 const TodoList = artifacts.require("./TodoList.sol");
+// Kiểm tra danh sách việc cần làm
+
 
 contract('TodoList', (accounts) => {
+    // Hiển thị các tài khoản trong blockchain
+
     before(async () => {
         this.todoList = await TodoList.deployed();
+        // Mỗi lần chạy có 1 bản sao việc cần làm khi triển khai các chuỗi block
     });
 
     it('deploys successfully', async () => {
@@ -11,6 +16,7 @@ contract('TodoList', (accounts) => {
         assert.notEqual(address, '');
         assert.notEqual(address, null);
         assert.notEqual(address, undefined);
+        // kiểm tra xem hợp đồng đã được triển khai vào blockchain đúng cách hay chưa bằng cách kiểm tra địa chỉ của nó.
     });
 
     it('lists tasks', async () => {
@@ -21,6 +27,7 @@ contract('TodoList', (accounts) => {
         assert.equal(task.completed, false);
         assert.equal(taskCount.toNumber(), 1);
     });
+    //  kiểm tra xem hợp đồng thông minh có liệt kê nhiệm vụ đúng cách hay không 
 
     it('creates tasks', async() => {
         const result = await this.todoList.createTask('A new task');
@@ -31,7 +38,10 @@ contract('TodoList', (accounts) => {
         assert.equal(event.id.toNumber(), 2);
         assert.equal(event.content, 'A new task');
         assert.equal(event.completed, false);
+        // kiểm tra để đảm bảo rằng sự kiện này được kích hoạt bất cứ khi nào một tác vụ mới được tạo
+        // đảm bảo rằng sự kiện đã được kích hoạt đúng cách
     });
+    
 
     it('toggles task completion', async () => {
         const result = await this.todoList.toggleCompleted(1);
@@ -41,6 +51,8 @@ contract('TodoList', (accounts) => {
         const event = result.logs[0].args;
         assert.equal(event.id.toNumber(), 1);
         assert.equal(event.completed, true);
+        
+        //Đánh dấu hoàn thành công việc
     })
 
 });
