fbee4c98ad4427342240a1bc9a5870a9231be5c3
NguyễnVănĐoàn <86102398+Doanlly@users.noreply.github.com>
2022-01-24 21:03:14 +0700

    update UI

--- market app/src/components/Navbar.js
+++ market app/src/components/Navbar.js
@@ -4,18 +4,23 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className=" navbar-dark bg-success flex-md-nowrap p-0 shadow">
+
+
         <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          className=" navbar navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          MARKET APP
+          <b><em>SE07-24.1</em></b>
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-white"><span id="account">{this.props.account}</span></small>
+            
+            <p className= "text-white"> <em><b> Tài khoản: </b></em>
+             <small className= "text-white"> <em><span id= "account">{ this.props.account}</span></em></small>
+            </p>
           </li>
         </ul>
       </nav>
